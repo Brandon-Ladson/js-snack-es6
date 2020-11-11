be496d0b5d5eb8dc57bbd82cd2765cc69979748c
Brandon Ladson <brandonladson1999@gmail.com>
2020-11-11 14:26:10 +0100

    es jsnack2 applicazione ES6

---
+++ jsnack 2/js/script.js
@@ -0,0 +1,42 @@
+// Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
+// una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
+
+// array di partenza
+const arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+
+console.log(arrayObj);
+
+// nuovo array in cui inserire una copia degli oggetti presenti in arrayOb aggiungendo la proprietà position
+const arrayObjCopia = [];
+
+// ciclo for per ciclare l'array di partenza
+for (let i = 0; i < arrayObj.length; i++) {
+
+  var nuovoOggetto = {...arrayObj[i], position: generatoreLettereRandom()};
+
+  arrayObjCopia.push(nuovoOggetto);
+
+}
+
+console.log(arrayObjCopia);
+
+// FUNZIONI
+// funzione per generare lettere random
+function generatoreLettereRandom() {
+
+  var letteraRandom;
+
+  const listaLettere = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for (let i = 0; i < listaLettere.length; i++) {
+    letteraRandom = listaLettere.charAt(Math.floor(Math.random() * listaLettere.length));
+  }
+
+  return letteraRandom;
+
+}
